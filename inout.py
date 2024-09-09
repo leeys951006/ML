50c8e5efb62b49db353b6d41334bcaa3aef297b4
이연승 <leeys951006@gmail.com>
2024-09-09 13:01:19 +0900

    ML

---
+++ inout.py
@@ -0,0 +1,6 @@
+my_name = "이연승"
+
+def print_name(name):
+  print(f"내이름은{name}입니다.")
+  
+print_name(my_name)
---
+++ testOne.py
@@ -0,0 +1,25 @@
+a = "이연승"
+b = "하이"
+
+def string_merge():
+  global a,b
+  return a+b
+
+print(string_merge())
+
+# * 맨날 이름쓰기 힘듦
+def example():
+  return "예제"
+
+anther_example = lambda: "예제"
+
+array = ["정호연", "고까미", "고겨울"]
+
+obj = {
+  "이연호": "안녕",
+  "똘망이": "멍멍",
+  "호연정": "제티충"
+}
+
+another_obj = ("이근태", "장성숙", "뗭호연")
+
